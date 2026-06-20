e7559de26409bbe9505e9c353e726be9531588f1
ttiramisu <ttiramisu10@gmail.com>
2026-06-20 22:45:19 +0800

    hhh

--- src/pages/AllWriteups.tsx
+++ src/pages/AllWriteups.tsx
@@ -43,7 +43,7 @@ export default function AllWriteups() {
           transition={{ duration: 0.55 }}
           className="section-title max-w-2xl"
         >
-          Markdown CTF writeups
+          CTF writeups
         </motion.h2>
 
         <motion.p
